daca723378a131d5261de6c392aa3123501a5c69
Jadiê <jadieoliveira00@gmail.com>
2020-03-26 22:32:58 -0300

    migration

--- backend/src/database/migrations/20200325124537_create_ongs.js
+++ backend/src/database/migrations/20200325124537_create_ongs.js
@@ -1,12 +1,11 @@
 
 exports.up = function (knex) {
-  knex.schema.createTable('ongs', function (table) {
+  return knex.schema.createTable('ongs', function (table) {
     table.string('id').primary();
     table.string('name').notNullable();
     table.string('email').notNullable();
     table.string('whatsapp').notNullable();
     table.string('city').notNullable();
-    table.string('name').notNullable();
     table.string('uf', 2).notNullable();
 
   })
@@ -14,6 +13,6 @@ exports.up = function (knex) {
 };
 
 exports.down = function (knex) {
-  knex.schema.dropTable('ongs')
+  return knex.schema.dropTable('ongs')
 
 };
